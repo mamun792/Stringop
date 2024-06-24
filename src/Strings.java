96101f08cb233095c561fb3580c443572c13a6ea
Mahabubur Rahaman <mahababurrahaman2014@gmail.com>
2024-06-25 00:20:51 +0600

    added string function

--- src/Strings.java
+++ src/Strings.java
@@ -1,7 +1,27 @@
 public class Strings {
   public static void main(String[] args) {
-    String st="mmuig";
-    System.out.println(st.length());
-    System.out.println(st.charAt(3));
+    String st = "mmuig";
+
+    // char[] ch = new char[10];
+    // for (int i = 0; i < st.length(); i++) {
+    //   ch[i] = st.charAt(i); 
+    // }
+
+    char[] ch = st.toCharArray();
+
+    for (int i = 0; i < st.length(); i++) {
+      System.out.print(ch[i]);
+    }
+    String substr = st.substring(0,3);
+    String trimmed = "  hello  ".trim();  // "hello"
+
+
+
+    System.out.println();
+    System.out.println("String Length: "+st.length());
+    System.out.println("index "+st.charAt(3));
+    System.out.println("Substring: "+ substr);
+    System.out.println("Triam : "+trimmed);
+
   }
 }
